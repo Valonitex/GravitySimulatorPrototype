62c76970086929b09291cfe0df9731e0b5cd4f21
Rudra <rudras5210@gmail.com>
2025-11-09 17:12:52 +0530

    Add project files.

---
+++ GravitySimulatorPrototype/EndBrace.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <array>
+#include <algorithm>
+#include <functional>
+#include <thread>
+#include <chrono>
+
+
+#define LOG(x) std::cout << x << std::endl;
---
+++ GravitySimulatorPrototype/main.cpp
@@ -0,0 +1,7 @@
+#include "EndBrace.h"
+
+int main()
+{
+	LOG("Hellow World");
+	std::cin.get();
+}
